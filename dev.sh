655e060a70838362142058bcaf3f9cd4f3a55a9a
Alex Margarit <alex@alxm.org>
2019-11-11 21:59:29 -0800

    Update .gitignore.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-geany -i build/make/* trap75/* dev.sh README.md &
+geany -i build/make/* trap75/* dev.sh README.md .gitignore &
 arduino trap75/trap75.ino &
 gimp -n assets/gfx/palette.png &
 cd build/make
